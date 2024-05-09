c05127f3af2a8b1cf70e774d2c75e95c371e69f3
Wasan A Yang <evol1996@outlook.com>
2024-05-09 23:34:41 +0700

    init auth

--- khai-tour-api/src/index.ts
+++ khai-tour-api/src/index.ts
@@ -1,5 +1,13 @@
 import * as functions from 'firebase-functions';
 import { NestServer } from './nest-server';
+
+const runtimeOptionals: functions.RuntimeOptions = {
+  memory: '256MB',
+  timeoutSeconds: 200,
+  minInstances: 0,
+  maxInstances: 1,
+};
 export const api = functions
-  .runWith({ memory: '256MB', timeoutSeconds: 200 })
+  .region('us-central1')
+  .runWith(runtimeOptionals)
   .https.onRequest(NestServer);
--- khai-tour-api/src/nest-server.ts
+++ khai-tour-api/src/nest-server.ts
@@ -6,12 +6,13 @@ import * as cors from 'cors';
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+const corsOptions = { origin: true };
 
 export const NestServer: express.Express = express();
 /* Express middleware. */
 NestServer.use(express.json());
 NestServer.use(express.urlencoded({ extended: false }));
-NestServer.use(cors());
+NestServer.use(cors(corsOptions));
 /* End of express middleware. */
 
 const startNestApplication = async (expressInstance: express.Express) => {
